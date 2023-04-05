57b8bd68f890f1efc46138d3fea8581775b80482
austin <austinmcclarren7@gmail.com>
2023-04-05 12:15:40 -0400

    throwaway commit

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Section from "./components/Section";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Footer/>
+      <Section/>
+    </div>
+  );
+}
+
+export default App;
